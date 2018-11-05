b7040e289eee88a34d4c64d5b4d18e6fd5f584a1
James Bear <jamesgrizzly@gmail.com>
2018-11-05 17:18:13 +0800

    Create log file

--- my_trainable.py
+++ my_trainable.py
@@ -2,6 +2,9 @@
 from trainable import TrainableBase
 
 class MyTrainable(TrainableBase):
-    def __init__(self):
-        super(MyTrainable, self).__init__()
+    def __init__(self, parser):
+        super(MyTrainable, self).__init__(parser)
         print("MyTrainable: initializing..")
+
+    def train(self):
+        print("MyTrainable: `train` not implemented..")
--- trainable.py
+++ trainable.py
@@ -5,6 +5,7 @@ import sys
 import hashlib
 import os
 import inspect
+import datetime
 
 def md5(fname):
     hash_md5 = hashlib.md5()
@@ -14,22 +15,51 @@ def md5(fname):
     return hash_md5.hexdigest()
 
 class TrainableBase:
-    def __init__(self):
+    def __init__(self, parser):
         print("TrainableBase: initializing..")
+        self.parser = parser
 
     def train(self):
-        pass
+        print("TrainableBase: `train` unimplemented..")
 
     def log_all(self):
-        pass
+        print("TrainableBase: `log_all` unimplemented..")
 
     def modify(self):
-        pass
+        print("TrainableBase: `modify` unimplemented..")
+
+    def run(self):
+        if self.parser.action == 'train':
+            self.create_log_file()
+            self.train()
+            self.log_all()
+            self.log_file.close()
+        else:
+            print('Unimplemented action:', self.parser.action)
+
+    def get_log_dir(self):
+        return "trainable_logs"
+
+    def get_log_file_name(self):
+        now = datetime.datetime.now()
+        time_str = now.strftime('%Y%m%d_%H%M%S.%f.txt')
+        time_str = self.parser.action + time_str
+        return time_str
+
+    def create_log_file(self):
+        log_dir = self.get_log_dir()
+        if not os.path.isdir(log_dir):
+            os.mkdir(log_dir)
+        log_file_name = self.get_log_file_name()
+        log_file_path = os.path.join(log_dir, log_file_name)
+        self.log_file = open(log_file_path, 'w')
+        print("log file created:", log_file_path)
+
 
 class MiniArgParser:
     def __init__(self):
         self.action = ''
-        self.options = []
+        self.options = {}
         self.unparsed = []
     
     def parse(self):
@@ -46,7 +76,7 @@ class MiniArgParser:
                 if cur[0] == '-' and cur[1] != '-' and i + 1 < num_args:
                     option_name = cur[1:]
                     option_value = sys.argv[i+1]
-                    self.options.append((option_name, option_value))
+                    self.options[option_name] = option_value
                     i += 2
                     continue
             elif cur.startswith('--'):
@@ -58,23 +88,25 @@ class MiniArgParser:
                     option_name = cur[2:]
                     option_value = ''
                 i += 1
-                self.options.append((option_name, option_value))
+                self.options[option_name] = option_value
                 continue
             break
         self.unparsed = sys.argv[i:]
 
         print("Action: ", self.action, ", options: ", self.options, ", unparsed:", self.unparsed)
 
-def GetTrainable():
-    currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+def GetTrainable(currentdir, parser):
     sys.path.insert(0, currentdir)
     from my_trainable import MyTrainable
-    trainable = MyTrainable()
+    trainable = MyTrainable(parser)
     return trainable
 
 if __name__ == '__main__':
-    trainable = GetTrainable()
     parser = MiniArgParser()
     parser.parse()
+    currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    trainable = GetTrainable(currentdir, parser)
+    os.chdir(currentdir)
+    trainable.run()
 
     
